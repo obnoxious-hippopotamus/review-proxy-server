e76bdba0cf8bc6e7559ee1607355417e792c61c6
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-24 13:29:16 -0600

    added proxy routes for each service

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const path = require('path');
 
@@ -9,6 +10,54 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../', '/client/public')));
+
+//JIM'S ROUTES ===============
+
+app.use('/api/movie', createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+}));
+
+app.use('/api/titles', createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+}));
+
+//BRANDON'S ROUTES ===========
+
+app.use('/api/reviews/sort/*', createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+}));
+
+app.use('/api/reviews', createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+}));
+
+app.use('/api/reviews/*', createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+}));
+
+app.use('/api/movies/*', createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+}));
+
+
+//JUSTIN'S ROUTES ============
+
+app.use('/api/descriptions', createProxyMiddleware({
+    target: 'http://localhost:3003',
+    changeOrigin: true,
+}));
+
+app.use('/api/similars', createProxyMiddleware({
+    target: 'http://localhost:3003',
+    changeOrigin: true,
+}));
+
 
 const PORT = process.env.PORT || 3000;
 
